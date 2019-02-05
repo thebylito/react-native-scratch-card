781928d8bc53bea2414fcf9e5752eee8c05df98e
Paul Patterson <pddpatterson@gmail.com>
2019-02-05 12:11:16 +0200

    Adds Proptype for brushSize to react class

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { RNScratchCard } = NativeModules;
 const iface = {
   name: 'RNScratchCardManager',
   propTypes: {
+    brushSize: PropTypes.number,
     color: PropTypes.string,
     maxPercent: PropTypes.number,
     ...ViewPropTypes
